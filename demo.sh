a5344fd85ce41f019a30f68d9d789db78d2662cb
shisi <zshisi14@gmail.com>
2019-02-17 20:27:22 +0800

    x修改

--- demo.sh
+++ demo.sh
@@ -1,3 +1,7 @@
+if [ -d $1 ]; then
+  echo 'error: dir exists'
+  exit
+else
 mkdir $1
 cd $1
 mkdir css js
@@ -7,4 +11,4 @@ echo "<!DOCTYPE>
  echo "h1{color: red;}">css/style.css
  echo "var string = "Hello World"
        alert(string)">js/main.js
-exit 0
+exit 
